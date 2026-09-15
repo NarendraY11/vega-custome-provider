9112d3a1f1cf660876216847c79c99bdf83a73e9
Narendra Yadav <146088759+NarendraY11@users.noreply.github.com>
2026-09-15 22:50:12 +0530

    Add provider distribution: catalog

---
+++ dist/download-pornhd-brazzers-2019-2020-watch/catalog.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {value: true});
+const catalog = [
+  { title: "Home", filter: "https://en8.pornhd8k.me/" },
+  { title: "Don&#39;t Break Me", filter: "https://en8.pornhd8k.me/category/don-t-break-me" },
+  { title: "Pervs On Patrol", filter: "https://en8.pornhd8k.me/category/pervs-on-patrol" },
+  { title: "Dirty Masseour", filter: "https://en8.pornhd8k.me/category/dirty-masseur" },
+  { title: "Brazzers en Espa&#241;ol", filter: "https://en8.pornhd8k.me/category/brazzers-en-espanol" },
+  { title: "I Know That Girl", filter: "https://en8.pornhd8k.me/category/i-know-that-girl" },
+  { title: "RK Prime", filter: "https://en8.pornhd8k.me/category/rk-prime" },
+  { title: "First Time Auditions", filter: "https://en8.pornhd8k.me/category/first-time-auditions" },
+  { title: "Big Naturals", filter: "https://en8.pornhd8k.me/category/big-naturals" },
+  { title: "Stranded Teens", filter: "https://en8.pornhd8k.me/category/stranded-teens" },
+  { title: "Mike&#39;s Apartment", filter: "https://en8.pornhd8k.me/category/mike-s-apartment" },
+  { title: "Happy Tugs", filter: "https://en8.pornhd8k.me/category/happy-tugs" },
+  { title: "Hot And Mean", filter: "https://en8.pornhd8k.me/category/hot-and-mean" },
+  { title: "Girls Gone Pink", filter: "https://en8.pornhd8k.me/category/girls-gone-pink" },
+  { title: "Moms Lick Teens", filter: "https://en8.pornhd8k.me/category/moms-lick-teens" }
+];
+exports.catalog = catalog;
+exports.genres = [];
